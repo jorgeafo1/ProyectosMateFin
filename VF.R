3c76f439bfd489554ebaf172e7a5a05ab7880304
jorgeafo1 <2350467j@umich.mx>
2024-09-20 18:47:44 -0600

    Carga tarea V1
    En esta carga se sube la primera versión de la tarea de interés simple

---
+++ VF.R
@@ -0,0 +1,10 @@
+# Se presentan las funciones de interés simple.
+# Autor: Jorge Alberto Ferreyra Ortiz.
+
+# Fórmula de valor fúturo con interés simple
+
+VaFiSi=function(VP, r, t)
+  exit=VP*(1+(r*t)){
+    
+    return(exit)
+  }
